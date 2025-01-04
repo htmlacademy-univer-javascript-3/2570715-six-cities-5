3635f3f0494c2663596b526a65c87f0dd42ac0ae
AIM1rage <michael.dev4242@gmail.com>
2025-01-04 14:19:19 +0500

    Add SortOptions component

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,5 +1,7 @@
 import {createAction} from '@reduxjs/toolkit';
 import {City, Offer} from '@/api/types.ts';
+import {SortOption} from '@/constants/sort-option.ts';
 
 export const updateOffers = createAction<{ offers: Offer[] }>('setOffers');
 export const updateCity = createAction<{ city: City }>('setCity');
+export const updateSortOption = createAction<{ sortOption: SortOption }>('setSortOptions');
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,11 +1,13 @@
 import {createReducer} from '@reduxjs/toolkit';
 import {cities} from '@/mocks/cities.ts';
 import {offers} from '@/mocks/offers.ts';
-import {updateCity, updateOffers} from '@/store/actions.ts';
+import {updateCity, updateOffers, updateSortOption} from '@/store/actions.ts';
+import {SortOption} from '@/constants/sort-option.ts';
 
 const initialState = {
   city: cities[5],
   offers: offers.filter((offer) => offer.city.name === cities[5].name),
+  sortOption: SortOption.Default,
 };
 
 export const reducer = createReducer(initialState, (builder) => {
@@ -15,5 +17,8 @@ export const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(updateOffers, (state, action) => {
       state.offers = action.payload.offers;
+    })
+    .addCase(updateSortOption, (state, action) => {
+      state.sortOption = action.payload.sortOption;
     });
 });
